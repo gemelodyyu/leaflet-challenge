7c21ac68f4be37866d3c086e59760ea9a722ebb2
gemelodyyu <yuge.chang0512@gmail.com>
2020-11-12 18:45:41 -0600

    update

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,7 +1,12 @@
 var myMap = L.map("map", {
-    center: [38.9717, -95.2353],
+    center: [39.7392, -104.9903],
     zoom: 5
   });
+
+// // Define a markerSize function that will give each city a different radius based on its population
+// function markerSize(depth) {
+//     return depth / 2;
+//   }
   
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -13,3 +18,32 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(myMap);
   
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"; 
+  
+d3.json(url, function(data) {
+    // console.log(data);
+    var featureData = data.features;
+    console.log(featureData);
+
+    // Define array to hold created markers
+    // var markers = []; 
+
+    for (var i=0; i<featureData.length; i++) {
+        var location = featureData[i].geometry.coordinates; 
+        // console.log(location)
+
+        if (location) {
+
+            // markers.push(
+                L.circle([location[1],location[0]], {
+                    stroke: false,
+                    fillOpacity: 0.75,
+                    color: "white",
+                    fillColor: "red",
+                    radius: parseInt(location[2])
+                }).bindPopup("<h3>" + featureData[i].properties.place +
+                "</h3><hr><p>" + new Date(featureData[i].properties.time) + "</p>").addTo(myMap); 
+        }
+
+    }
+
+}); 
